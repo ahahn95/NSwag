08e43e49c5a95d52024849ffe690c69cce408696
Alexander Hahn <ahahn95@gmail.com>
2021-01-28 21:17:32 -0500

    meme

--- AlexHahn.NSwagTest/ClientApp/src/App.tsx
+++ AlexHahn.NSwagTest/ClientApp/src/App.tsx
@@ -1,30 +1,36 @@
 ﻿import React from 'react';
-import {useGetAllCars} from "./hooks/cars/carHooks";
+import {useCreateCar, useGetAllCars} from "./hooks/cars/carHooks";
 import {CarRequest} from "./client";
 
 export const App = () => {
     const [cars, refresh] = useGetAllCars();
-    
-    console.log(cars);
-    
+    const [createCar] = useCreateCar();
+
     const handleSubmit = async (event: any) => {
         event.preventDefault()
         const values = new FormData(event.target);
-        
+
         const car: CarRequest = {
-            
+            make: values.get("make")?.toString() || '',
+            model: values.get("model")?.toString() || '',
         }
+
+        await createCar(car);
+        await refresh();
     }
-    
-    return <div>
+
+    return (
+        <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor={"make"}>Make</label>
-                <input id={"make"} name={"make"} />
+                <input id={"make"} name={"make"}/>
                 <label htmlFor={"model"}>model</label>
-                <input id={"model"} name={"model"} />
+                <input id={"model"} name={"model"}/>
                 <button type={"submit"}>Submit</button>
             </form>
-        <button type={"button"} onClick={refresh}>Update</button>
-            </div>;
+            <ul>
+                {cars.map(car => <li key={car.id}>{car.make} {car.model}</li>)}
+            </ul>
+        </div>);
 }
 
